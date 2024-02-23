013cf68ed3a863793cbf81ed46162ee5a9dc86ff
rafiul33 <rafiulislam2629@gmail.com>
2024-02-23 00:18:58 -0500

    modal added

--- js/price-calculator.js
+++ js/price-calculator.js
@@ -1,14 +1,16 @@
-function priceCalculator(id) {
-  console.log(id)
+function priceCalculator(id, item) {
   const itemPrice = getPrice(id);
   const totalPrice = getPrice('total-price');
   const total = getPrice('total');
   const currentTotalPrice = (totalPrice + itemPrice).toFixed(2);
   if (currentTotalPrice >= 200) {
-    const btn = buttonApply();
+    buttonApply('btn-apply');
   };
+  // For button Make Purchase
+  buttonApply('make-purchase');
   setPrice('total-price', currentTotalPrice);
   setPrice('total', currentTotalPrice);
+  selectedItems(item);
 };
 
 // get discount
@@ -36,14 +38,13 @@ function setPrice(elementId, newPrice){
 };
 
 // button apply
- function buttonApply() {
-  const btnApply = document.getElementById('btn-apply');
+ function buttonApply(id) {
+  const btnApply = document.getElementById(id);
   btnApply.disabled = false;
   return true;
 };
 // get input value
 function getInputValue() {
-  
   const inputValueElement = document.getElementById('input-coupon');
   const inputValue = inputValueElement.value;
   const totalPrice = getPrice('total-price');
@@ -60,6 +61,28 @@ function getInputValue() {
   }
   inputValueElement.value = '';
 };
+// selected items
+function selectedItems(item) {
+  const selectedItems = document.getElementById('selected-items');
+  const count = selectedItems.childElementCount;
+  const p = document.createElement('p');
+  p.classList.add('my-3');
+  p.innerHTML = `<span class="text-xl">${count + 1} ${item}</>`
+  selectedItems.appendChild(p);
+};
+
+// Display Congratulations
+
+// function displayCongrats(){
+//   console.log('cc')
+//   const sectionModal = document.getElementById('section-modal');
+//   console.log(sectionModal)
+//   sectionModal.style.display = 'block';
+// }
+
+
+
+
 
 
 
